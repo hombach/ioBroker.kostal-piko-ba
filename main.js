44b9cade8b298a418a909858035d00f26f1eba53
Christian Hombach <christian@homba.ch>
2020-05-24 23:47:59 +0200

    23:47

--- main.js
+++ main.js
@@ -306,7 +306,7 @@ class KostalPikoBA extends utils.Adapter {
 
 //        let AutoRun = window.setInterval(this.ReadPiko(), 1000);
         //clearInterval(AutoRun);
-        var sched10 = schedule.schedulejob('*/10 * * * * *', this.ReadPiko());
+        //var sched10 = schedule.schedulejob('*/10 * * * * *', this.ReadPiko());
 
     }
 
