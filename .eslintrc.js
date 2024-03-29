ccb844155703b585daef7d02185cfe7f46f900bc
Hombach <GIT@homba.ch>
2024-03-29 10:42:00 +0100

    3.0.11

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+	root: true, // Don't look outside this project for inherited configs
+    parserOptions: {
+		ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:prettier/recommended", // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    ],
+    plugins: [],
+    env: {
+        "es6": true,
+        "node": true,
+        "mocha": true
+    },
+    rules: {
+        "indent": [
+            "error",
+            4,
+            {
+                "SwitchCase": 1
+            }
+        ],
+        "no-console": "off",
+        "no-var": "error",
+        "prefer-const": "error",
+        "quotes": [
+            "error",
+            "single",
+            {
+                "avoidEscape": true,
+                "allowTemplateLiterals": true
+            }
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "prettier/prettier": [
+			"error",
+			{
+				endOfLine: "auto",
+			},
+		]
+    },
+}
