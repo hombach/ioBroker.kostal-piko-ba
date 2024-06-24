2d4bc4701cb25e40c63e75bf24983942aefc228a
Hombach <GIT@homba.ch>
2024-06-24 21:52:25 +0200

    4.0.1

--- test/package.js
+++ test/package.js
@@ -1,8 +1,5 @@
-import { join } from 'path';
-// WiP const path = require('path');
-import { tests } from '@iobroker/testing';
-// WiP const { tests } = require('@iobroker/testing');
+const path = require('path');
+const { tests } = require('@iobroker/testing');
 
 // Validate the package files
-tests.packageFiles(join(__dirname, '..'));
-// WiP tests.packageFiles(path.join(__dirname, '..'));
+tests.packageFiles(path.join(__dirname, '..'));
