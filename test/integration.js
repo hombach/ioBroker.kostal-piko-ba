0bccb0de924f3a320f7bf787f1f34d6d60e7b260
Hombach <GIT@homba.ch>
2024-06-24 21:54:12 +0200

    4.0.1

--- test/integration.js
+++ test/integration.js
@@ -1,8 +1,5 @@
-import { join } from 'path';
-// WiP const path = require('path');
-import { tests } from '@iobroker/testing';
-// WiP const { tests } = require('@iobroker/testing');
+const path = require('path');
+const { tests } = require('@iobroker/testing');
 
 // Run integration tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
-tests.integration(join(__dirname, '..'));
-// WiP tests.integration(path.join(__dirname, '..'));
+tests.integration(path.join(__dirname, '..'));
