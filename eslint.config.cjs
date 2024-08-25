9e6f5fde4543eff7579e6b6bc76f0dd04e63f05b
Hombach <GIT@homba.ch>
2024-08-25 13:46:32 +0200

    4.1.4

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,43 +1,57 @@
-const globals = require('globals');
-const js = require('@eslint/js');
+const globals = require("globals");
+const js = require("@eslint/js");
 
-const {FlatCompat} = require('@eslint/eslintrc');
+const { FlatCompat } = require("@eslint/eslintrc");
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all,
 });
 
-module.exports = [{
-    ignores: ['**/build/', '**/.prettierrc.js', '**/.eslintrc.js'],
-}, ...compat.extends('eslint:recommended', 'plugin:prettier/recommended'), {
-    plugins: {},
+module.exports = [
+	{
+		ignores: ["**/build/", "**/.prettierrc.js", "**/.eslintrc.js"],
+	},
+	...compat.extends("eslint:recommended", "plugin:prettier/recommended"),
+	{
+		plugins: {},
+		languageOptions: {
+			globals: {
+				...globals.node,
+				...globals.mocha,
+			},
+			ecmaVersion: 2022,
+			sourceType: "commonjs",
+		},
 
-    languageOptions: {
-        globals: {
-            ...globals.node,
-            ...globals.mocha,
-        },
-        ecmaVersion: 2022,
-        sourceType: 'commonjs',
-    },
-
-    rules: {
-        indent: ['error', 4, {
-            SwitchCase: 1,
-        }],
-        'no-console': 'off',
-        quotes: ['error', 'single', {
-            avoidEscape: true,
-            allowTemplateLiterals: true,
-        }],
-        semi: ['error', 'always'],
-        'prettier/prettier': ['error', {
-            endOfLine: 'auto',
-        }],
-        'no-var': 'error',
-        'prefer-const': 'error',
-        'no-trailing-spaces': 'error',
-    },
-}];
+		rules: {
+			indent: [
+				"error",
+				"tab",
+				{
+					SwitchCase: 1,
+				},
+			],
+			"no-console": "off",
+			quotes: [
+				"error",
+				"double",
+				{
+					avoidEscape: true,
+					allowTemplateLiterals: true,
+				},
+			],
+			semi: ["error", "always"],
+			"prettier/prettier": [
+				"error",
+				{
+					endOfLine: "auto",
+				},
+			],
+			"no-var": "error",
+			"prefer-const": "error",
+			"no-trailing-spaces": "error",
+		},
+	},
+];
