0a0bd073170559f086105a828125aa8af3580e9d
Hombach <GIT@homba.ch>
2024-08-25 19:35:14 +0200

    4.1.4

--- test/unit.js
+++ test/unit.js
@@ -1,8 +1,8 @@
-import { join } from 'path';
+import { join } from "path";
 // WiP const path = require('path');
-import { tests } from '@iobroker/testing';
+import { tests } from "@iobroker/testing";
 // WiP const { tests } = require('@iobroker/testing');
 
 // Run unit tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
-tests.unit(join(__dirname, '..'));
+tests.unit(join(__dirname, ".."));
 // WiP tests.unit(path.join(__dirname, '..'));
