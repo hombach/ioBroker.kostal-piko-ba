c161297d3d98dd303e51e66eebb04c03314d656f
Hombach <GIT@homba.ch>
2024-08-26 02:41:05 +0200

    4.2.0

---
+++ test/integration.js
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const path = require("path");
+const { tests } = require("@iobroker/testing");
+
+// Run integration tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
+tests.integration(path.join(__dirname, ".."), {
+	allowedExitCodes: [11],
+});
--- test/integration.mjs
+++
@@ -1,7 +0,0 @@
-import { join } from "path";
-import { tests } from "@iobroker/testing";
-
-// Run integration tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
-tests.integration(join(__dirname, ".."), {
-	allowedExitCodes: [11],
-});
